0d1fe0336630d638f94c796d189cf1f004d1d012
chaochao <138183913+chaochaomiao@users.noreply.github.com>
2025-07-26 08:16:30 +0800

    Updateutil.go
    update
    
    Signed-off-by: chaochao <138183913+chaochaomiao@users.noreply.github.com>

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -202,9 +202,10 @@ do:
 		"origin":        "https://www.123pan.com",
 		"referer":       "https://www.123pan.com/",
 		"authorization": "Bearer " + d.AccessToken,
-		"user-agent":    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) openlist-client",
-		"platform":      "web",
-		"app-version":   "3",
+		"user-agent":    "123pan/v2.5.4(Android_14.0.0;Xiaomi)",
+		"app-version": "77",
+		"platform": "android",
+		"x-app-version": "2.5.4",
 		//"user-agent":    base.UserAgent,
 	})
 	if callback != nil {
